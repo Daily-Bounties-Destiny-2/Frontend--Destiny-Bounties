b5590a5e7a329c318a46a3f15f3454f1de1a2f53
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-08-22 16:29:06 -0700

    added home and bounties pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route
+} from 'react-router-dom';
+import Header from './Header.js';
+import HomePage from './HomePage.js';
+import DailyBounties from './DailyBounties.js';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+    <Route render={({ location, history }) => (
+        <React.Fragment>
+            <SideNav
+                onSelect={(selected) => {
+                    const to = '/' + selected;
+                    if (location.pathname !== to) {
+                        history.push(to);
+                    }
+                }}
+            >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Home
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="bounties">
+                        <NavIcon>
+                            <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
+                        </NavIcon>
+                        <NavText>
+                            Daily Bounties
+                        </NavText>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
+              <Header />
+            <main>
+              <div className='content'>
+                <Route path="/" exact component={props => <HomePage />} />
+                <Route path="/bounties" component={props => <DailyBounties />} />
+              </div>
+                
+            </main>
+        </React.Fragment>
+    )}
+    />
+</Router>
+            </div>
+        )
+    }
 }
-
-export default App;
